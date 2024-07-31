5517b4661b93408a473618568139eb1b42649a2e
Rafael Loustaunau <rlousta13@gmail.com>
2024-07-31 11:56:45 -0500

    Initial Commit

---
+++ DessertAPI/DessertAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertAPIApp.swift
+//  DessertAPI
+//
+//  Created by user264773 on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
